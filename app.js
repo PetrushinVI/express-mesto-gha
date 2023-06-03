1b22a5dc4ae1f8e56a01fcd133e477233365f29b
Владислав <alina220578@mail.ru>
2023-06-03 17:10:54 +0300

    pr14

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const router = require('./routes/index');
-const rateLimit = require('express-rate-limit');
+const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
+const rateLimit = require('express-rate-limit');
+const router = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
 const { createUser, login } = require('./controllers/users');
 const { signInValidation, signUpValidation } = require('./middlewares/validation');
-const { errors } = require('celebrate');
+
 
 const app = express();
-app.use(express.json());
+
+
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
@@ -17,16 +20,22 @@ const limiter = rateLimit({
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
+app.use(express.json());
+
+app.use(cookieParser());
+
 const { PORT = 3000 } = process.env;
 
 app.post('/signup', signUpValidation, createUser);
 app.post('/signin', signInValidation, login);
 
-app.use(router);
 app.use(limiter);
-app.use(cookieParser());
-app.use(errorHandler);
+
+app.use(router);
+
 app.use(errors());
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`App listening on Port: ${PORT}`);
